b2e253c47e6405463fda286f07ee8f1a8adca1de
a.s.galecki@gmail.com <a.s.galecki@gmail.com>
2021-12-15 18:19:01 +0100

    Create getLatestRecipes fn

---
+++ app/src/service/getLatestRecipes.ts
@@ -0,0 +1,21 @@
+import IRecipe from '../ts/interfaces/IRecipe';
+
+const sortRecipes = (array: IRecipe[]) => {
+  array.sort(function (a: IRecipe, b: IRecipe) {
+    const c = Number(new Date(a.date));
+    const d = Number(new Date(b.date));
+    return c - d;
+  });
+};
+
+const getLatestRecipes = (
+  recipes: IRecipe[],
+  numberOfLatestRecipes: number
+) => {
+  const givenRecipes = recipes.slice();
+  sortRecipes(givenRecipes);
+  const result = givenRecipes.slice(0, numberOfLatestRecipes);
+  return result;
+};
+
+export default getLatestRecipes;
---
+++ app/src/tests/service/getLatestRecipes.test.ts
@@ -0,0 +1,13 @@
+import allRecipes from '../../mocks/data/allRecipes.json';
+import getLatestRecipes from '../../service/getLatestRecipes';
+
+// interfaces
+import IRecipe from '../../ts/interfaces/IRecipe';
+
+test('sort recipes by date and return explicit number of last of them', () => {
+  const { recipes } = allRecipes;
+  const latestRecipes: IRecipe[] = getLatestRecipes(recipes, 2);
+
+  expect(latestRecipes).toHaveLength(2);
+  expect(latestRecipes[0].name).toStrictEqual('Canadian Butter Tarts');
+});
